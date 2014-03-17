9c5173d1874b29dcb40f34365fd3ad1275da3022
Danny Tizon <dtizon@gmail.com>
2014-03-17 14:58:00 -0700

    added logback

--- src/main/java/com/gapperdan/hellomongo/domain/Main.java
+++ src/main/java/com/gapperdan/hellomongo/domain/Main.java
@@ -3,6 +3,7 @@ package com.gapperdan.hellomongo.domain;
 import com.gapperdan.hellomongo.model.Gender;
 import com.gapperdan.hellomongo.model.Person;
 import com.gapperdan.hellomongo.service.PersonService;
+import com.gapperdan.hellomongo.util.Logger;
 
 public class Main {
 
@@ -11,6 +12,7 @@ public class Main {
         try {
             PersonService personService = new PersonService();
             Person person;
+            Logger log = new Logger();
 
             personService.deleteAll();
 
@@ -21,6 +23,9 @@ public class Main {
             person = Person.create("Jane", "Doer", Gender.FEMALE, 29);
             personService.add(person);
 
+            log.info("added persons");
+            log.info(personService.getAll().toString());
+
             System.out.println("added persons:");
             System.out.println(personService.getAll());
 
@@ -28,11 +33,17 @@ public class Main {
             person = personService.searchByName("Bruce", "Wayne");
             System.out.println("found: "+person.toString());
 
-            System.out.println("updating Bruce Wayne's age to 199");
+            log.info("searching for Bruce Wayne");
+            log.info("found: "+person.toString());
+
+            //System.out.println("updating Bruce Wayne's age to 199");
             personService.updateAge("Bruce", "Wayne", 199);
             System.out.println("after update:");
             person = personService.searchByName("Bruce", "Wayne");
             System.out.println("found: "+person.toString());
+
+            log.info("after update");
+            log.info("found :"+person.toString());
 
         } catch (Exception e) {
             e.printStackTrace();
---
+++ src/main/java/com/gapperdan/hellomongo/util/Logger.java
@@ -0,0 +1,12 @@
+package com.gapperdan.hellomongo.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Logger {
+
+    public void info(String str) {
+        log.info(str);
+    }
+
+}
